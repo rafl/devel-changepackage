efc803eea0032ae298fa0c4e19643501f2140a36
Florian Ragwitz <rafl@debian.org>
2010-10-09 03:21:53 +0200

    Add docs

--- lib/Devel/ChangePackage.pm
+++ lib/Devel/ChangePackage.pm
@@ -2,12 +2,32 @@ use strict;
 use warnings;
 
 package Devel::ChangePackage;
+# ABSTRACT: Change the package core is currently being compiled in
 
 use XSLoader;
 use Sub::Exporter -setup => {
     exports => ['change_package'],
     groups  => { default => ['change_package'] },
 };
+
+=head1 SYNOPSIS
+
+    package Foo;
+
+    use Devel::ChangePackage;
+
+    BEGIN { change_package 'Bar' }
+
+    warn __PACKAGE__; # Bar
+
+=func change_package
+
+    my $previous_package = change_package $new_package;
+
+Changes the package code is being compiled in to C<$new_package>. The name of
+the package things were compiled in before is returned.
+
+=cut
 
 XSLoader::load(
     __PACKAGE__,
