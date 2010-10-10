d12df659ead3f3ed923ad8bf5669f3e87db8a8a7
Florian Ragwitz <rafl@debian.org>
2010-10-10 05:40:08 +0200

    Improve the docs

--- lib/Devel/ChangePackage.pm
+++ lib/Devel/ChangePackage.pm
@@ -20,6 +20,64 @@ use Sub::Exporter -setup => {
 
     warn __PACKAGE__; # Bar
 
+=head1 DESCRIPTION
+
+This module allows you to tell perl what package code should be compiled
+into. It's very similar to the C<package> statement, but allows you to use
+variables and therefore to generate the package name to be used in any way you
+want. Just like C<package>, this module only has compile-time effects.
+
+An important difference between this module and the C<package> statement is that
+the latter is lexically scoped, while the former is not. For example:
+
+    {
+        package Foo;
+        # in package 'Foo'
+
+        {
+            package Bar;
+            # in package 'Bar'
+
+            BEGIN { Devel::ChangePackage::change_package('Affe') }
+            # in package 'Affe'
+        }
+
+        # back in the 'Foo' package
+    }
+
+One way to think about this is changing the "argument" provided for the last
+C<package> statement, or, if there was none yet, changing the default namespace
+from C<main> to something else. It's not possible to make changes to the current
+namespace outside of the currently compiling scope.
+
+=head1 EXAMPLE
+
+With this module, it's possible to write the following code:
+
+    package namespace::anon;
+
+    use Devel::ChangePackage;
+
+    my $i = 0;
+    sub import {
+        change_package 'namespace::anon::' . $i++;
+    }
+
+Which you could later use like:
+
+    use namespace::anon; # "package namespace::anon::0;"
+    sub foo { ... } # namespace::anon::0::foo
+
+    use namespace::anon; # "package namespace::anon::1;"
+    sub foo { ... } # namespace::anon::1::foo
+
+
+That is, every C<use namespace::anon> will create a new "anonymous" package
+under the C<namespace::anon::> namespace and make that the current package. All
+code that is compiled afterwards, until the next change of package (by either
+the C<package> statement or C<change_package>) will live in that anonymous
+namespace.
+
 =func change_package
 
     my $previous_package = change_package $new_package;
